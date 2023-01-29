e643f4821c07b9ad45ae27b6e1b82c4b94d4ac58
stfg.prof <stfg.prof@gmail.com>
2023-01-29 17:36:17 -0300

    arreglando url de imgs en iteracion-fractal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,21 @@
 import Layout from "components/layout";
 import Link from "next/link";
-import CarouselParent from "@/components/carouselparent"
+import Carousel from "@/components/carousel"
 import { Children } from "react";
 
-function RowList({ children }) {
-  return (
-    <div className="RowList">
-      {Children.map(children, (child, index) => {
-        return (
-          <div className="Row">
-            {index}
-            {child}
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+const images = ['/imgs/micrones/04.jpg', '/imgs/micrones/05.jpg',
+   '/imgs/micrones/06.jpg','/imgs/micrones/07.jpg','/imgs/micrones/10.jpg','/imgs/micrones/12.jpg',
+   '/imgs/micrones/14.jpg',];
 
-function Parent({ children }) {
-  // children.className=" text-5xl";
-  return <div className="mt-10 bg-orange-500">{children}</div>;
-}
 
-const NotFound = () => {
+const HomePage = () => {
   return (
     <>
       <Layout>
         <div className="flex justify-center p-1/6">
           <div className="mt-4 unreset w-11/12 md:w-3/4 text-center text-5xl">
             aca va la bio
-           
+           <Carousel images={images} />
             <br />
             <Link href="/haceres">obras</Link>
           </div>
