f8f43afee9766a83f15b609d2536057470d5d8de
stfg.prof <stfg.prof@gmail.com>
2023-01-30 22:25:52 -0300

    acentuaciones y correciones de estilo en el contenido

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,8 +33,8 @@ const About = () => {
                 )</p>
 
 
-            <div className=" w-full">
-              <p className="m-auto md:w-2/3 text-lg  text-justify ">
+            <div className=" w-full pt-5 ">
+              <p className="m-auto md:w-2/3 text-lg  text-justify py-2">
                 Soy artista visual, programador e ilustrador, maestrando de
                 Artes Electrónicas en la UNTREF. Actualmente estoy en vías de
                 desarrollo de una tesis de investigación relacionada a la
@@ -45,12 +45,12 @@ const About = () => {
                 como integrante de “Núcleo” en La Tangente para el VII
                 aniversario de “Structured Collective”.{" "}
               </p>
-              <p className="m-auto md:w-2/3 text-lg  text-justify ">
+              <p className="m-auto md:w-2/3 text-lg  text-justify py-2">
                 Como ilustrador he participado en el taller de Monica Weiss,
                 pudiendo ser parte de la muestra FIEYO en la Feria Internacional
                 del libro de Buenos Aires 2022.{" "}
               </p>
-              <p className="m-auto md:w-2/3 text-lg  text-justify ">
+              <p className="m-auto md:w-2/3 text-lg  text-justify py-2">
                 Busco combinar mis saberes de programación orientados al Live
                 Coding con prácticas de expresión corporal, habilitando lugares
                 de encuentro y creación colectiva.
